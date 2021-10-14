ba12cc02f9f250ad63b386edee6da123224ae88c
Lenny <60232469+lenny45155@users.noreply.github.com>
2021-10-14 11:47:02 -0300

    :tada: Challange Finalized

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
-
 import '../styles/tasklist.scss'
-
 import { FiTrash, FiCheckSquare } from 'react-icons/fi'
 
 interface Task {
   id: number;
+
   title: string;
   isComplete: boolean;
 }
@@ -15,15 +14,31 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => task.id === id? {
+      ...task,
+      isComplete: !task.isComplete
+    } :task);
+    setTasks(newTasks)
   }
+
+
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id);
+    setTasks(filteredTasks)
   }
 
   return (
@@ -48,6 +63,9 @@ export function TaskList() {
         <ul>
           {tasks.map(task => (
             <li key={task.id}>
+              
+              
+              
               <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
                 <label className="checkbox-container">
                   <input 
@@ -64,7 +82,13 @@ export function TaskList() {
               <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
                 <FiTrash size={16}/>
               </button>
-            </li>
+            
+               
+               
+               
+               
+               
+               </li>
           ))}
           
         </ul>
